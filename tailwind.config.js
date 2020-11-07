308b4d122836e2b3902c73bb3fd2ba14181e3697
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-07 18:33:23 +0800

    add misc files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ["Roboto", "sans-serif"],
+            },
+            height: {
+                40: "40vh",
+                50: "50vh",
+                60: "60vh",
+                30: "30vh",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
